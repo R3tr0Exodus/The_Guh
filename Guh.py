f4344ae2eb1218a0cce5d8a78f3df24628fa0dcc
R3tr0 <toby@soegaards.dk>
2023-02-05 01:43:46 +0100

    Tilføjede en limiter til hvor stort et tal kan indtaste som input.

--- Guh.py
+++ Guh.py
@@ -1,20 +1,29 @@
+Limit = 1000000
+
 navnet = input('Skriv navn: ')
-print('Dit navn er', navnet,'?','Ad...')
+print('Dit navn er', navnet,'?','Ad... Anyways, lad os ligge 2 tal sammen')
 
 
 while True: #loop indtil du giver int
-    etTal = input("Gib Number: ") #dit input
+    etTal = input("Gib Number:\n") #dit input
     try:
         val = int(etTal) #Er dit value input et nummer/int?
-        break;
+        if val <= Limit: #checker for om dit input val er mindre en det tal skrevet ind
+            break
+        else:
+            print('For mange tal. Max input er', Limit)
     except ValueError:
-        print('det er ikke et nummer din kejle') #It's not an int
+            print('Det er ikke et nummer din kejle') #It's not an int
+
 
 while True: #Same before
-    etGodTal = input('Gib Another Number: ') #dit andet input
+    etGodTal = input('Gib Another Number:\n') #dit andet input
     try:
         val = int(etGodTal) #er dit andet input et nummer/int?
-        break;
+        if val <= Limit: #checker for om dit input val er mindre en det tal skrevet ind
+            break
+        else:
+            print('For mange tal. Max input er', Limit)
     except ValueError:
         print('Det er ikke et nummer din kejle') #it's not an input... Do you not learn from your mistakes?
 
